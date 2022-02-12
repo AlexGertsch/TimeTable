848933bd6e952121c554c1e6f3f8d4bf387a7209
Alexander Gertsch <alexander.gertsch@unterstrass.edu>
2022-02-12 12:50:07 +0100

    Initial Commit

---
+++ TimeTable/TimeTableApp.swift
@@ -0,0 +1,17 @@
+//
+//  TimeTableApp.swift
+//  TimeTable
+//
+//  Created by stundenplan on 12.02.22.
+//
+
+import SwiftUI
+
+@main
+struct TimeTableApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
